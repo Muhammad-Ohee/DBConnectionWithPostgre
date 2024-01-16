1c4dec045597cc5c1e7f44a8e2f38eb74983f553
Muhammad-Ohee <md.ohee86@gmail.com>
2024-01-16 08:35:46 +0600

    Add project files.

---
+++ DBConnection/Data/DBContext.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DBConnection.Data
+{
+    class AppDbContext : DbContext
+    {
+        protected readonly IConfiguration Configuration;
+
+        public AppDbContext(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            //base.OnConfiguring(optionsBuilder);
+            optionsBuilder.UseNpgsql(Configuration.GetConnectionString("WebApiDatabase"));
+        }
+
+        public DbSet<Student> Students { get; set; }
+    }
+}
---
+++ DBConnection/Data/Student.cs
@@ -0,0 +1,11 @@
+﻿namespace DBConnection.Data
+{
+    public class Student
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public string Department { get; set; }
+        public float Mark { get; set; }
+    }
+}
